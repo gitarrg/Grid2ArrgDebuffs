665424ebe6e2d37eb1d08276e662656b27bf9f1b
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2023-04-14 17:51:56 +0100

    fixes

--- options/Grid2Options.lua
+++ options/Grid2Options.lua
@@ -4,26 +4,30 @@ local _, ns = ...
 -- Register the Options with the Grid2 Addon
 --------------------------------------------------------------------------------
 
-Grid2Options:RegisterStatusOptions(
-	"arrg-debuffs", -- type
-	"debuff",       -- categoryKey
+Grid2.OnOptionsLoad = Grid2.OnOptionsLoad or {}
 
-	function(self, status, options, optionParams)
+Grid2.OnOptionsLoad["arrg-debuffs"] = function()
+	Grid2Options:RegisterStatusOptions(
+		"arrg-debuffs", -- type
+		"debuff",       -- categoryKey
 
-		ns.gui_refresh()
+		function(self, status, options, optionParams)
 
-		options.debuffs_tab = {
-			type = "group",
-			name = "Debuffs",
-			order = 10,
-			args = ns.GUI,
+			ns.gui_refresh()
+
+			options.debuffs_tab = {
+				type = "group",
+				name = "Debuffs",
+				order = 10,
+				args = ns.GUI,
+			}
+		end, -- end funcMakeOptions
+
+		-- optionParams
+		{
+			titleIcon = 133074,
+			hideTitle = true,
+			masterStatus = "arrg-debuffs-1",
 		}
-	end, -- end funcMakeOptions
-
-	-- optionParams
-	{
-		titleIcon = 133074,
-		hideTitle = true,
-		masterStatus = "arrg-debuffs-1",
-	}
-)
+	)
+end
